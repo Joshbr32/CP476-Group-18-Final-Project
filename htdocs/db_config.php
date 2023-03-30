fdbfee35f43c01ab809e785fce469a6dffc01d55
Josh Dhori <Joshbr32@gmail.com>
2023-03-30 17:10:52 -0400

    Finished all functionality code

--- htdocs/db_config.php
+++ htdocs/db_config.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['username']) && isset($_POST['password'])) {
-        $_SESSION['username'] = $_POST['username'];
-        $_SESSION['password'] = $_POST['password'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+        $_SESSION["username"] = $_POST["username"];
+        $_SESSION["password"] = $_POST["password"];
 
-        header("Location: students_list.php");
+        header("Location: upload_data.php");
         exit();
     }
 }
